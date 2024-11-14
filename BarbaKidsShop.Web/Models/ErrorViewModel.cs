2561df30ba6d08e230aeb7b23bc3c6b1ea0af057
Radoslav <Radoslav@DESKTOP-OC22MTD>
2024-11-14 12:57:46 +0200

    Add project files.

---
+++ BarbaKidsShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BarbaKidsShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
